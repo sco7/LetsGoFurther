e9b7b8b671fe78d59ca885070ac18bab45763dff
sco7 <sco77.townson@gmail.com>
2024-06-20 08:05:30 +0100

    letsgo-3.2

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write([]byte(js))
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered an internal error and could not process your request.", http.StatusInternalServerError)
+	}
 }
